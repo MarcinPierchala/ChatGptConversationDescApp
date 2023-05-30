3ec9c376d24ff84d8d0a422a135a37db1bba6b7c
MarcinPierchala <68757012+MarcinPierchala@users.noreply.github.com>
2023-05-30 20:28:01 +0200

    itIsAllWhatIHaveDoneTillNow

---
+++ MyAI_Chat/MyAI_Chat/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MyAI_Chat
+{
+    partial class ConversationWithChatBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rUNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.apiKeyArea = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chatArea = new System.Windows.Forms.TextBox();
+            this.loadApiKeyBtn = new System.Windows.Forms.Button();
+            this.sTOPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1350, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rUNToolStripMenuItem,
+            this.sTOPToolStripMenuItem,
+            this.aBOUTToolStripMenuItem,
+            this.eXITToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1350, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rUNToolStripMenuItem
+            // 
+            this.rUNToolStripMenuItem.Name = "rUNToolStripMenuItem";
+            this.rUNToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.rUNToolStripMenuItem.Text = "RUN";
+            this.rUNToolStripMenuItem.Click += new System.EventHandler(this.rUNToolStripMenuItem_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.loadApiKeyBtn);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.apiKeyArea);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1350, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // apiKeyArea
+            // 
+            this.apiKeyArea.Enabled = false;
+            this.apiKeyArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.apiKeyArea.Location = new System.Drawing.Point(215, 6);
+            this.apiKeyArea.Name = "apiKeyArea";
+            this.apiKeyArea.Size = new System.Drawing.Size(948, 29);
+            this.apiKeyArea.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "YOUR API_KEY =>>>";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.chatArea);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 70);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1350, 439);
+            this.panel3.TabIndex = 2;
+            // 
+            // chatArea
+            // 
+            this.chatArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chatArea.Enabled = false;
+            this.chatArea.Location = new System.Drawing.Point(0, 0);
+            this.chatArea.Multiline = true;
+            this.chatArea.Name = "chatArea";
+            this.chatArea.Size = new System.Drawing.Size(1350, 439);
+            this.chatArea.TabIndex = 0;
+            // 
+            // loadApiKeyBtn
+            // 
+            this.loadApiKeyBtn.Enabled = false;
+            this.loadApiKeyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loadApiKeyBtn.Location = new System.Drawing.Point(1169, 6);
+            this.loadApiKeyBtn.Name = "loadApiKeyBtn";
+            this.loadApiKeyBtn.Size = new System.Drawing.Size(178, 28);
+            this.loadApiKeyBtn.TabIndex = 2;
+            this.loadApiKeyBtn.Text = "LOAD_API_KEY";
+            this.loadApiKeyBtn.UseVisualStyleBackColor = true;
+            this.loadApiKeyBtn.Click += new System.EventHandler(this.loadApiKeyBtn_Click);
+            // 
+            // sTOPToolStripMenuItem
+            // 
+            this.sTOPToolStripMenuItem.Name = "sTOPToolStripMenuItem";
+            this.sTOPToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.sTOPToolStripMenuItem.Text = "STOP";
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.aBOUTToolStripMenuItem.Text = "ABOUT";
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            // 
+            // ConversationWithChatBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 619);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ConversationWithChatBot";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rUNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sTOPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button loadApiKeyBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox apiKeyArea;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox chatArea;
+    }
+}
+
---
+++ MyAI_Chat/MyAI_Chat/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChatGpt.Net;
+
+namespace MyAI_Chat
+{
+    public partial class ConversationWithChatBot : Form
+    {
+        public ConversationWithChatBot()
+        {
+            InitializeComponent();
+        }
+
+        private void rUNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.apiKeyArea.Enabled = true;
+            this.loadApiKeyBtn.Enabled = true;
+        }
+
+        private void loadApiKeyBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var bot = new ChatGpt(apiKeyArea.Text)
+            }
+            if (apiKeyArea.Text.Length > 0)
+            {
+                this.chatArea.Enabled = true;
+
+                chatArea.Text += "Hello. This is a simple ChatBot_conversation_app." + "\r\n" + "Please type the message and push 'ENTER_BUTTON'." + "\r\n" + "Have a good time:-)";
+            }
+        }
+    }
+}
